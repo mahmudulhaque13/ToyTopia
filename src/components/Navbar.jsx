4be55261b5ec96086a181b17880f850723fccc32
mahmudulhaque13 <m.h.upal13@gmail.com>
2026-03-26 12:41:51 +0600

    Change text color of Navbar title

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
         <MyContainer className="flex items-center justify-between py-2">
           <figure>
             {/* <img src="" alt="" /> */}
-            <h3 className="font-bold text-lg text-orange-400">ToyTopia</h3>
+            <h3 className="font-bold text-lg text-orange-500">ToyTopia</h3>
           </figure>
           <ul className="flex items-center gap-2.5 font-semibold">
             <li>
