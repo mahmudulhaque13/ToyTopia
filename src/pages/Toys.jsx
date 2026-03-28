af776a6061a7771bc330892ba4ff2c3d48e80a28
mahmudulhaque13 <m.h.upal13@gmail.com>
2026-03-28 19:26:11 +0600

    Adjust grid gap and margin for Toys page

--- src/pages/Toys.jsx
+++ src/pages/Toys.jsx
@@ -15,7 +15,7 @@ const Toys = () => {
           <div className="flex justify-between my-6">
             <h1 className=" font-bold text-xl">All Toys</h1>
           </div>
-          <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5 my-5 place-items-center">
+          <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 my-6 place-items-center">
             {toys.map((toy) => (
               <ToyCard key={toy.toyId} toy={toy}></ToyCard>
             ))}
